5b3f50a2a68e9f6eb4500cefe43a875e4bb1aafe
Jakob Kampichler <jkampich1411@users.noreply.github.com>
2021-06-30 22:37:40 +0200

    remlabel not working; add works as expected
    Going now to sleep

--- index.js
+++ index.js
@@ -2,20 +2,43 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+const commands = require("probot-commands");
+
 module.exports = (app) => {
   // Your code here
   app.log.info("App got loaded POG");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+  commands(app, "addlabel", (context, command) => {
+    const labels = command.arguments.split(/, */);
+    const text = context.issue({body: `Added labels: '${labels}'`});
+    returnStuff(text, labels);
+
+    function returnStuff(textToSend, labels) {
+      let createCom = context.octokit.issues.createComment(textToSend);
+      let addLabs = context.octokit.issues.addLabels(context.issue({ labels }));
+      return createCom && addLabs;
+    }
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  commands(app, "remlabel", (context, command) => {
+    const labels = command.arguments.split(/, */);
+    const text = context.issue({body: `Removed labels: '${labels}'`});
+    returnStuff(text, labels);
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+    function returnStuff(textToSend, labels) {
+      let createCom = context.octokit.issues.createComment(textToSend);
+      let log = console.log(labels);
+      let remLabs = context.octokit.issues.removeLabel(context.issue({ labels }));
+      return createCom &&log && remLabs;
+    }
+  })
+
+  app.on("issues.opened", async (context) => {
+    const { body } = context.payload.issue;
+    
+    const comment = context.issue({
+      body: body.includes("[jcutil:thanks]") ? "You are welcome!" : "Thanks!"
+    });
+    return context.octokit.issues.createComment(comment);
+  });
+}
